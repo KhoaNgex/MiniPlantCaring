7867c1dd90899f7fa392c5e6c94955c9fd83cb82
khoacaotrananh <94796856+khoacaotrananh@users.noreply.github.com>
2023-03-30 00:42:20 +0700

    Updated!
    App.js -> Kết nối đến StartScreen, Chưa tạo kết nối với SettingScreen (Đăng xuất trả về StartScreen)

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,29 @@
 import React from "react";
-import Main from "./navigation/main";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import StartScreen from "./navigation/screens/StartScreen";
+import LoginScreen from "./navigation/screens/LoginScreen";
+
+const Start = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Main />
+      <NavigationContainer>
+        <Start.Navigator initialRouteName={StartScreen} animation="slide_from_right">
+          <Start.Screen 
+              name="StartScreen"
+              component={StartScreen}
+              options={{
+                headerShown: false,
+              }}/>
+          <Start.Screen 
+              name="LoginScreen"
+              component={LoginScreen}
+              options={{
+                headerShown: false,
+              }}/>
+        </Start.Navigator>
+      </NavigationContainer>
   );
 }
 
